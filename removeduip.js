9a40f647d3802992ce50cfca284d06eb22b5e1cf
PriyankagithubS <priyankashanmugasundaramr@gmail.com>
2024-05-22 22:20:05 +0530

    add

--- removeduip.js
+++ removeduip.js
@@ -1,16 +1,12 @@
-const arrayWithDuplicates = [1, 2, 2, 3, 4, 4, 5];
-const uniqueArray = Array.from(new Set(arrayWithDuplicates));
-console.log(uniqueArray); // Output: [1, 2, 3, 4, 5]
-const arrayWithDuplicates = [1, 2, 2, 3, 4, 4, 5];
-const uniqueArray = arrayWithDuplicates.filter((value, index, array) => {
-    return array.indexOf(value) === index;
-});
-console.log(uniqueArray); // Output: [1, 2, 3, 4, 5]
-const arrayWithDuplicates = [1, 2, 2, 3, 4, 4, 5];
-const uniqueArray = arrayWithDuplicates.reduce((accumulator, currentValue) => {
+
+
+
+
+let arrayWithDuplicates = [1, 2, 2, 3, 4, 4, 5];
+let uniqueArray = arrayWithDuplicates.reduce((accumulator, currentValue) => {
     if (!accumulator.includes(currentValue)) {
         accumulator.push(currentValue);
     }
     return accumulator;
 }, []);
-console.log(uniqueArray); // Output: [1, 2, 3, 4, 5]
+console.log(uniqueArray); 
